927c63abc84730f903725bac2f96d3e9d1a1377b
James Ricarte <uhenyou@gmail.com>
2024-10-26 15:14:28 +0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/get', (req, res) => {
+    res.json("hello");
+})
+
+const mongo_uri = "mongodb+srv://admin:jamesuhenyou123@cluster0.0p7jv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongo_uri)
+.then(() => {
+
+    console.log("Mongodb Connection Successfull");
+
+    app.listen(5000, () => {
+        console.log('server running at port 5000');
+    })
+})
+.catch((error) => {
+    console.log(error);
+})
